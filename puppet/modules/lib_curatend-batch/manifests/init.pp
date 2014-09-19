ebb258e912f3d6c5df3887ea415cae9771a2b5d3
msuhovec <suhovecky@nd.edu>
2014-09-19 13:10:46 -0400

    add curatend-batch dir creation, delete templates

--- puppet/modules/lib_curatend-batch/manifests/init.pp
+++ puppet/modules/lib_curatend-batch/manifests/init.pp
@@ -1,6 +1,23 @@
-class lib_curate::standalone {
+#
+# This class is called from Capistrano, and has the overall layout
+# of the batch server
 
-include lib_home_app
+class lib_curatend-batch() {
+
+# create app user, build ruby
+
+include lib_app_home
 include lib_ruby
+
+# app subdirectory for batch
+
+file { "/home/app/curatend-batch": 
+	ensure => directory,
+	mode => 0755,
+	owner => "app",
+	group => "app",
+	require => Class["lib_app_home"],
+
+}
 
 }
