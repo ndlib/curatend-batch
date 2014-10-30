8482cef3f4728857d7a8a81b5eb9ab68eefd0aaf
Don Brower <dbrower@nd.edu>
2014-10-30 17:17:42 -0400

    Add characterize task

--- Gemfile
+++ Gemfile
@@ -3,8 +3,9 @@ source 'https://rubygems.org'
 # This should be everything except :deploy; And by default, we mean any of
 # the environments that are not used to execute the deploy scripts
 group :default do
- gem 'rake'
- gem 'rof', git: 'https://github.com/ndlib/rof'
+  gem 'rake'
+  gem 'rof', git: 'https://github.com/ndlib/rof'
+  gem 'resque', '~> 1.23'
 end
 
 group :deploy do
---
+++ tasks/add-to-queue.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+require('base64')
+require('resque')
+
+# This adds a characterize job to the queue for every id on stdin
+# (ids are separated by newlines)
+
+class CharacterizeJob
+  def initialize(pid)
+    @pid = pid
+  end
+
+  def queue_name
+    :characterize
+  end
+end
+
+module Sufia
+  module Resque
+    class Queue
+      def push(job)
+        queue = job.queue_name
+        ::Resque.enqueue_to queue, MarshaledJob, Base64.encode64(Marshal.dump(job))
+      end
+    end
+
+    class MarshaledJob
+    end
+  end
+end
+
+redis_host_port = ENV['REDIS_HOST_PORT']
+if redis_host_port.nil?
+  abort("REDIS_HOST_PORT unset")
+end
+
+Resque.redis = redis_host_port
+queue = Sufia::Resque::Queue.new
+
+STDIN.each do |line|
+  queue.push(CharacterizeJob.new(line))
+end
+
