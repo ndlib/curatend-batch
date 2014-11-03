3140f0b28b9b21f1f55e52ad5f2113456ac61dda
Don Brower <dbrower@nd.edu>
2014-11-03 16:34:21 -0500

    Update puppet for with additional config vars

--- puppet/modules/lib_batchs/manifests/init.pp
+++ puppet/modules/lib_batchs/manifests/init.pp
@@ -3,6 +3,10 @@ class lib_batchs( $batchs_root = '/opt/batchs') {
 	$pkglist = [ "golang" ]
 	$batch_queue_dir = hiera('batch_queue_dir')
 	$fedora_passwd = hiera('fedora_passwd')
+	$batch_curate_url = hiera('batch_curate_url')
+	$batch_fedora_url = hiera('batch_fedora_url')
+	$batch_noid_pool = hiera('batch_noid_pool')
+	$batch_redis_host_port = hiera('batch_redis_host_port')
 
 	package { $pkglist:
 		ensure => present,
@@ -13,8 +17,8 @@ class lib_batchs( $batchs_root = '/opt/batchs') {
 		require => Package["$pkglist"],
 	}
 
-	exec { "Build-batchs-from-repo":  
-		command => "/bin/bash -c \"export GOPATH=${batchs_root} && go get github.com/ndlib/curatend-batch\"",	
+	exec { "Build-batchs-from-repo":
+		command => "/bin/bash -c \"export GOPATH=${batchs_root} && go get github.com/ndlib/curatend-batch\"",
 		unless => "/usr/bin/test -f ${batchs_root}/bin/batchs",
 		require => File[$batchs_root],
 	}
@@ -50,7 +54,7 @@ class lib_batchs( $batchs_root = '/opt/batchs') {
 	exec { "stop-batchs":
 		command => "/sbin/initctl stop batchs",
 		unless => "/sbin/initctl status batchs | grep stop",
-		require => File['logrotate.d/batchs'], 
+		require => File['logrotate.d/batchs'],
 	}
 
 	exec { "start-batchs":
@@ -58,5 +62,5 @@ class lib_batchs( $batchs_root = '/opt/batchs') {
 		unless => "/sbin/initctl status batchs | grep process",
 		require => Exec["stop-batchs"]
 	}
-		
+
 }
