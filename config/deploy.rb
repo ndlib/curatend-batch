3f9eee017c201d49635f3c62a83c464ee44c2dff
msuhovec <suhovecky@nd.edu>
2016-03-01 17:44:38 -0500

    added /etc/puppet/environments/production/modules to puppet moduleptah in deploy
    #4

--- config/deploy.rb
+++ config/deploy.rb
@@ -112,7 +112,7 @@ namespace :und do
   def run_puppet(options={})
     local_module_path = File.join(release_path, 'puppet', 'modules')
     option_string = options.map { |k,v| "#{k} => '#{v}'" }.join(', ')
-    run %Q{sudo puppet apply --modulepath=#{local_module_path}:/global/puppet_standalone/modules:/etc/puppet/modules -e "class { 'lib_curatend_batch': #{option_string} }"}
+    run %Q{sudo puppet apply --modulepath=#{local_module_path}:/global/puppet_standalone/modules:/etc/puppet/modules:/etc/puppet/environments/production/modules -e "class { 'lib_curatend_batch': #{option_string} }"}
   end
 
   desc "Run puppet using the modules supplied by the application"
