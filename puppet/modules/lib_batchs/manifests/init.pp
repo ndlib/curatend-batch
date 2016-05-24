a5230dae40a3e755ea71a745473f74b2cb45c60f
Don Brower <dbrower@nd.edu>
2016-05-24 12:37:30 -0400

    Use runit to manage batchs daemon
    * Add runit to puppet manifest.
    * Remove upstart service. Redo puppet dependency notation.
    * Remove all the "su -u app" commands from the task scripts.
    
    issue #22

--- puppet/modules/lib_batchs/manifests/init.pp
+++ puppet/modules/lib_batchs/manifests/init.pp
@@ -1,5 +1,7 @@
 class lib_batchs( $batchs_root = '/opt/batchs') {
 
+	include lib_runit
+
 	$pkglist = [
 		"golang",
 		"jq"
@@ -16,43 +18,37 @@ class lib_batchs( $batchs_root = '/opt/batchs') {
 
 	package { $pkglist:
 		ensure => present,
-	}
+	} ->
 
-	file { [ "$batchs_root", "${batchs_root}/log", "${batchs_root}/src/github.com/ndlib" ]:
-		ensure => directory,
-		require => Class['lib_go::build'],
-	}
-
-    # symlink to whatever the jenkis capistrano deploy checked out
-    # (to support the deploy tag used by jenkins)
-    file { 'batch_go_source':
-         path => "${batchs_root}/src/github.com/ndlib/curatend-batch",
-         ensure => link,
-         target => "/home/app/curatend-batch/current",
-         force => true,
-         require => File["${batchs_root}/src/github.com/ndlib"],
-    }
-
-	exec { "Build-batchs-from-repo":
-		command => "/bin/bash -c \"export GOPATH=${batchs_root} && go get -d github.com/ndlib/curatend-batch && go install github.com/ndlib/curatend-batch\"",
-		require => File['batch_go_source'],
-	}
-
-        #install bendo bclient into batchs_root
-
+	# install bendo bclient into batchs_root
 	class { 'lib_go::build':
 		repo => 'github.com/ndlib/bendo',
 		goroot => "${batchs_root}",
 		target => "github.com/ndlib/bendo/cmd/bclient",
-		require => Package[$pkglist],
-	}
+	} ->
 
-	file { 'batchs.conf':
-		name => '/etc/init/batchs.conf',
-		replace => true,
-		content => template('lib_batchs/upstart.erb'),
-		require => Exec["Build-batchs-from-repo"],
-	}
+	file { "${batchs_root}/log":
+		ensure => directory,
+		owner => "app",
+		group => "app",
+	} ->
+
+	file { "${batchs_root}/src/github.com/ndlib":
+		ensure => directory,
+	} ->
+
+	# symlink to whatever the jenkis capistrano deploy checked out
+	# (to support the deploy tag used by jenkins)
+	file { 'batch_go_source':
+		path => "${batchs_root}/src/github.com/ndlib/curatend-batch",
+		ensure => link,
+		target => "/home/app/curatend-batch/current",
+		force => true,
+	} ->
+
+	exec { "Build-batchs-from-repo":
+		command => "/bin/bash -c \"export GOPATH=${batchs_root} && go get -d github.com/ndlib/curatend-batch && go install github.com/ndlib/curatend-batch\"",
+	} ->
 
 	file { 'batchs/tasks':
 		name => "${batchs_root}/tasks",
@@ -60,33 +56,74 @@ class lib_batchs( $batchs_root = '/opt/batchs') {
 		source => "${batchs_root}/src/github.com/ndlib/curatend-batch/tasks",
 		recurse => true,
 		purge => true,
-		require => Exec['Build-batchs-from-repo'],
-	}
+	} ->
 
 	file { 'batchs/tasks/conf':
 		name => "${batchs_root}/tasks/conf",
 		replace => true,
 		content => template('lib_batchs/tasks.conf.erb'),
-		require => File['batchs/tasks'],
 	}
 
-	file { 'logrotate.d/batchs':
-		name => '/etc/logrotate.d/batchs',
+# Create batchs runit service directories
+
+	$batchrunitdirs = [ "/etc/sv/batchs", "/etc/sv/batchs/log" ]
+
+	file { $batchrunitdirs:
+		ensure => directory,
+		owner => "app",
+		group => "app",
+		require => Class[['lib_runit','lib_go::build']],
+	} ->
+
+	file { 'batchsrunitexec':
+		name => '/etc/sv/batchs/run',
+		owner => "app",
+		group => "app",
+		mode => '0755',
 		replace => true,
-		require => File["batchs/tasks/conf"],
-		content => template('lib_batchs/logrotate.erb'),
+		content => template('lib_batchs/run.erb'),
+		require => File[$batchrunitdirs],
+	} ->
+
+	file { 'batchsrunitlog':
+		name => '/etc/sv/batchs/log/run',
+		owner => "app",
+		group => "app",
+		replace => true,
+		mode => '0755',
+		content => template('lib_batchs/log_run.erb'),
 	}
 
-	exec { "stop-batchs":
+# make sure the upstart managed service is down and service file removed
+# (these puppets steps will be unnecessary once batchs has been
+# deployed to all the environments)
+
+	exec { 'stop-batchs-upstart':
 		command => "/sbin/initctl stop batchs",
-		unless => "/sbin/initctl status batchs | grep stop",
-		require => File['logrotate.d/batchs'],
+		returns => [0, 1],
+		require => File[$batchrunitdirs],
+	} ->
+	file { '/etc/init/batchs':
+		ensure => absent,
 	}
 
-	exec { "start-batchs":
-		command => "/sbin/initctl start batchs",
-		unless => "/sbin/initctl status batchs | grep process",
-		require => Exec["stop-batchs"]
-	}
+# Start the service and describe it to puppet
 
+	class { 'lib_runit::add':
+		service_name => "batchs",
+		service_path => "/etc/sv/batchs",
+		require => File[['batchsrunitlog', "${batchs_root}/log", "${batchs_root}/tasks/conf", '/etc/init/batchs']],
+	} ->
+
+	service { 'batchs':
+		provider => 'base',
+		ensure => running,
+		enable => true,
+		hasstatus => false,
+		hasrestart => false,
+		restart => '/sbin/sv restart batchs',
+		start => '/sbin/sv start batchs',
+		stop => '/sbin/sv stop batchs',
+		status => '/sbin/sv status batchs',
+	}
 }
