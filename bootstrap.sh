661839f55df13df83f90325832a705aa9d44fefd
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-05-26 10:12:33 -0400

    Automating config file setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,18 @@ BATCHDIR="$HOME/goapps/curatend-batch"
 
 # There can be multiple environments within the batch space
 mkdir -p "$BATCHDIR/development"
+
+# Setting up a config file
+config_file=tasks/conf
+if [ -f "$config_file" ]; then
+  echo "Config file already exists"
+else
+  echo "Creating a default config file"
+
+  # Escaping expanded $GOPATH
+  target=$(echo "$GOPATH" | awk '{gsub("/", "\\\\&");print}')
+
+  sed s/=\$GOPATH/="$target"/ "$config_file".example > "$config_file"
+fi
+
 curatend-batch -queue="$BATCHDIR/development" -tasks="$GOPATH/src/github.com/ndlib/curatend-batch/tasks"
