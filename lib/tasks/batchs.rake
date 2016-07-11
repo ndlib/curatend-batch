7cb44d4047f695139601f2a9db2b3401c76382e4
msuhovec <suhovecky@nd.edu>
2016-07-11 16:49:38 -0400

    initial commit of makefile, skeleton task
    #32

---
+++ lib/tasks/batchs.rake
@@ -0,0 +1,6 @@
+namespace :batchs do
+  desc 'yada yada yada'
+  task make_bendo_updatelist do
+    puts 'we made it this far'
+  end
+end
---
+++ script/sync_fedora_to_bendo.sh
@@ -0,0 +1,20 @@
+!/bin/bash
+
+# Try to get a lock, and exit if someone else already has it.
+# This keeps a sync processes from spawning nd overlapping
+# should a paricular sync take a long time.
+# The lock is released when this shell exits.
+
+cd /home/app/curatend-batch/current
+
+exec 200> "/tmp/sync_fedora_to_bendo"
+flock -e --nonblock 200 || exit 0
+
+# source our ruby env
+source /etc/profile.d/ruby.sh
+
+# source our app environment.
+
+#source /home/app/curatend-batch/shared/system/metrics-env.sh
+
+/opt/ruby/current/bin/bundle exec rake batchs:make_bendo_updatelist
